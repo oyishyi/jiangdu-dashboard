d7c473c1fa02312920abc98873ba8c9884b2b5e5
kerryDong11 <55862830+kerryDong11@users.noreply.github.com>
2022-06-04 20:20:16 +0800

    update

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,6 +1,6 @@
-const host = 'http://127.0.0.1:4000';
+//const host = 'http://127.0.0.1:4000';
 //const host = "http://kerrytest.natapp1.cc"
-
+var host = window.location.protocol+"//"+document.domain+":4000";
 export default{
     host,    //地址
  
